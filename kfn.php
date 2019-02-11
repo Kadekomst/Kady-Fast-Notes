f1041c48ea9b79b4cd7ab3f366e4c09d04bfb5e3
Kadekomst <38081711+Kadekomst@users.noreply.github.com>
2019-02-11 22:10:47 +0300

    Deleted a bunch of unnecessary comments
    Deleted a bunch of unnecessary comments

--- kfn.php
+++ kfn.php
@@ -328,56 +328,35 @@ if ( ! class_exists( 'KFN' ) ) :
 				'label'               => null,
 				'labels'              => array(
 					'name'               => __( 'Kady Fast Notes', 'kfn' ),
-					// основное название для типа записи
 					'singular_name'      => __( 'Kady Fast Notes', 'kfn' ),
-					// название для одной записи этого типа
 					'add_new'            => __( 'Add note', 'kfn' ),
-					// для добавления новой записи
 					'add_new_item'       => __( 'Add new note', 'kfn' ),
-					// заголовка у вновь создаваемой записи в админ-панели.
 					'edit_item'          => __( 'Edit note', 'kfn' ),
-					// для редактирования типа записи
 					'new_item'           => __( 'Note Content', 'kfn' ),
-					// текст новой записи
 					'view_item'          => __( 'View note', 'kfn' ),
-					// для просмотра записи этого типа.
 					'search_items'       => __( 'Search for note', 'kfn' ),
-					// для поиска по этим типам записи
 					'not_found'          => __( 'Not found', 'kfn' ),
-					// если в результате поиска ничего не было найдено
 					'not_found_in_trash' => __( 'Not found in trash', 'kfn' ),
-					// если не было найдено в корзине
 					'parent_item_colon'  => '',
-					// для родителей (у древовидных типов)
 					'menu_name'          => 'Kady Fast Notes',
-					// название меню
 				),
 				'description'         => '',
 				'public'              => true,
 				'publicly_queryable'  => null,
-				// зависит от public
 				'exclude_from_search' => null,
-				// зависит от public
 				'show_ui'             => null,
-				// зависит от public
 				'show_in_menu'        => null,
-				// показывать ли в меню адмнки
 				'show_in_admin_bar'   => null,
-				// по умолчанию значение show_in_menu
 				'show_in_nav_menus'   => null,
-				// зависит от public
 				'show_in_rest'        => null,
-				// добавить в REST API. C WP 4.7
 				'rest_base'           => null,
-				// $post_type. C WP 4.7
 				'menu_position'       => null,
 				'menu_icon'           => null,
-				//'capability_type'   => 'post',
-				//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-				//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+				//'capability_type'   => 'pos
+				//'capabilities'      => 'post',
+				//'map_meta_cap'      => null,
 				'hierarchical'        => false,
 				'supports'            => array( 'title', 'editor' ),
-				// 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
 				'taxonomies'          => array(),
 				'has_archive'         => false,
 				'rewrite'             => true,
