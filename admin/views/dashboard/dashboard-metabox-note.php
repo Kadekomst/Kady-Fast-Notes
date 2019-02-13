f1897d6dd5294bca4a057053f083a11c07f97899
Kadekomst <38081711+Kadekomst@users.noreply.github.com>
2019-02-13 15:22:03 +0300

    Updated viewes for KFN dashboard metabox
    Some fixes in HTML/CSS markup of KFN dashboard metabox views

--- admin/views/dashboard/dashboard-metabox-note.php
+++ admin/views/dashboard/dashboard-metabox-note.php
@@ -1,19 +1,18 @@
 <?php
 /**
  * dashboard-metabox-note.php
- * -----------------------------------------
- * HTML/CSS view for KFN dashboard metabox
- * -----------------------------------------
+ * ---------------------------------------------------
+ * HTML/CSS view for KFN dashboard metabox note block
+ * ---------------------------------------------------
+ * @author Kadekomst
  * @since 1.0.0
  */
-global $kfn, $kfn_options;
-$kfn->load_api_helpers();
-
 ?>
-<!---->
-<?php foreach( kfn_get_option('dashboard_metabox_notes') as $note ) : ?>
-<div class="kfn-note">
-	<h3 class="kfn-note-title"><?php echo $note['title']; ?></h3>
-	<p class="kfn-note-content"><?php echo $note['content']; ?></p>
-</div>
+
+<?php foreach ( get_option('kfn_dashboard_metabox_notes') as $note ) : ?>
+    <div class="kfn-dashboard-note">
+	    <h3 class="kfn-dashboard-note-title"><?php echo $note['title']; ?></h3>
+        <p class="kfn-dashboard-note-date"><?php echo $note['date']; ?></p>
+	    <p class="kfn-dashboard-note-content"><?php echo $note['content']; ?></p>
+    </div>
 <?php endforeach; ?>
--- admin/views/dashboard/dashboard-metabox.php
+++ admin/views/dashboard/dashboard-metabox.php
@@ -9,33 +9,45 @@
  */
 
 global $kfn, $kfn_dashboard_metabox;
-$kfn->load_api_helpers();
-
+$kfn->load_api();
 $params = kfn_get_option('dashboard_metabox_form_args');
+
 ?>
 
-<form action="" method="POST" class="<?php echo $params['form_class'] ?>">
+<form action=""
+      method="<?php echo $params['method'] ?>"
+      class="kfn-dashboard-metabox">
 
-	<label for="<?php echo $params['title_input']['name'] ?>">
+	<label for="<?php echo $params['title_input']['name'] ?>"
+           class="kfn-dashboard-label">
 		<?php echo $params['title_input']['label_text'] ?>
 	</label>
 
 	<input type="<?php echo $params['title_input']['type'] ?>"
 	       name="<?php echo $params['title_input']['name'] ?>"
-	       placeholder="<?php echo $params['title_input']['placeholder'] ?>">
+	       placeholder="<?php echo $params['title_input']['placeholder'] ?>"
+           class="kfn-dashboard-input">
 
-	<label for="<?php echo $params['content_input']['name'] ?>">
-		<?php echo $params['content_input']['label_text'] ?>
+	<label for="<?php echo $params['content_textarea']['name'] ?>"
+           class="kfn-dashboard-label">
+		<?php echo $params['content_textarea']['label_text'] ?>
 	</label>
 
-	<input type="<?php echo $params['content_input']['type'] ?>"
-	       name="<?php echo $params['content_input']['name'] ?>"
-	       placeholder="<?php echo $params['content_input']['placeholder'] ?>">
+    <textarea name="<?php echo $params['content_textarea']['name'] ?>"
+              cols="30"
+              rows="10"
+              class="kfn-dashboard-textarea"
+              placeholder="<?php echo $params['content_textarea']['placeholder'] ?>">
+    </textarea>
 
-	<input type="<?php echo $params['save_button']['type'] ?>"
-	       value="<?php echo $params['save_button']['text'] ?>"
-	       class="<?php  echo $params['save_button']['class'] ?>">
+	 <?php
+     // Submit button provided by WordPress API.
+     submit_button( __('Save') );
+     ?>
 
-    <?php $kfn_dashboard_metabox->display_notes(); ?>
+    <?php
+     // Display all registered notes
+     $kfn_dashboard_metabox->display_notes();
+    ?>
 
 </form>
