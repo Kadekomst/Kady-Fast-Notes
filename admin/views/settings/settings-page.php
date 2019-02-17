b491d2af6cb7acf55c335ab0984c1a66a7a9290d
Kadekomst <38081711+Kadekomst@users.noreply.github.com>
2019-02-17 18:45:31 +0300

    Views for plugin settings section and fields.
    Addition for previous commit. This update introduces views for plugin settings section and fields.

--- admin/views/settings/settings-page.php
+++ admin/views/settings/settings-page.php
@@ -1,3 +1,28 @@
 <?php
+/*
+ * settings-page.php
+ * -------------------------------------
+ * Part of Kady Fast Notes source code.
+ *
+ * This file contains HTML/CSS view
+ * for KFN settings page.
+ * -------------------------------------
+ * @author Kadekomst
+ * @since 1.0.0
+ */
+?>
 
-echo 'This is Kady Fast Notes settings page!';
+<div class="wrap">
+    <h1><?php _e('Kady Fast Notes', 'kfn'); ?></h1>
+    <form method="post" action="options.php">
+		<?php
+		// This prints out all hidden setting dashboard
+		settings_fields( 'kfn-settings-page' );
+		do_settings_sections( 'kfn-settings-page' );
+
+		// Submit button
+		submit_button();
+		?>
+    </form>
+</div>
+
