afab114eb647c8dbfc051c05fdebc4d52a207abe
Kadekomst <38081711+Kadekomst@users.noreply.github.com>
2019-02-18 17:59:25 +0300

    Updated KFN dashboard metabox views
    dashboard-metabox.php and dashboard-metabox-note.php files were updated according to introducing of new plugin settings page.

--- admin/views/dashboard/dashboard-metabox-note.php
+++ admin/views/dashboard/dashboard-metabox-note.php
@@ -2,41 +2,55 @@
 /**
  * dashboard-metabox-note.php
  * ---------------------------------------------------
- * HTML/CSS view for KFN dashboard metabox note block
+ * View for KFN dashboard metabox note block
  * ---------------------------------------------------
  * @author Kadekomst
  * @since 1.0.0
  */
 
+// Array of settings
+$settings = get_option('kfn_settings');
+
+// Arguments for WP_Query
+$numberposts = $settings['dashboard_metabox_notes_per_page'];
+$orderby = $settings['dashboard_metabox_orderby'];
+$order = $settings['dashboard_metabox_order'];
+
 // Notes array
 $notes = new WP_Query( array(
-	'numberposts'      => -1,
-	'orderby'          => 'date',
-	'order'            => 'DESC',
+	'posts_per_page'   => $numberposts,
+	'orderby'          => $orderby,
+	'order'            => $order,
 	'post_type'        => 'kfn',
 	'suppress_filters' => true
 ) );
 
-while ( $notes->have_posts() ) {
-	// Required post call
-	$notes->the_post();
+// Wrapper for notes
+?>
+<div class="kfn-dashboard-notes">
+    <?php
 
-	// Post contents
-	$post_title     = $notes->post->post_title;
-	$post_content   = $notes->post->post_content;
-	$post_date      = $notes->post->post_date;
-	$post_permalink = admin_url('post.php?post='.$notes->post->ID.'&action=edit');
+    while ( $notes->have_posts() ) {
 
-	?>
+        // Required post call
+        $notes->the_post();
 
-    <!-- Metabox note template  -->
-    <div class="kfn-dashboard-note">
-        <a href="<?php echo $post_permalink; ?>" class="kfn-dashboard-note-title"><?php echo $post_title; ?></a>
-        <p class="kfn-dashboard-note-date"><?php echo $post_date; ?></p>
-        <p class="kfn-dashboard-note-content"><?php echo $post_content; ?></p>
-    </div>
+        // Post contents
+        $post_title     = $notes->post->post_title;
+        $post_content   = $notes->post->post_content;
+        $post_date      = $notes->post->post_date;
+        $post_permalink = admin_url('post.php?post='.$notes->post->ID.'&action=edit');
 
-<?php
+        // Metabox note template
+        ?>
+        <div class="kfn-dashboard-note">
+            <a href="<?php echo $post_permalink; ?>" class="kfn-dashboard-note-title"><?php echo $post_title; ?></a>
+            <p class="kfn-dashboard-note-date"><?php echo $post_date; ?></p>
+            <p class="kfn-dashboard-note-content"><?php echo $post_content; ?></p>
+        </div>
 
-}
-wp_reset_postdata();
+        <?php
+        // Resetting post data.
+        } wp_reset_postdata();
+        ?>
+</div>
