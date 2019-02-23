b8cdcd91858cf65485977ee47f5ae022cd0b0eef
Kadekomst <38081711+Kadekomst@users.noreply.github.com>
2019-02-23 20:01:10 +0300

    Uninstalling functionality is introduced
    You can fully uninstall KFN plugin from your WordPress admin panel.

--- uninstall.php
+++ uninstall.php
@@ -11,18 +11,32 @@
  * @author Kadekomst
  */
 
-global $kfn;
-$kfn->load_api();
-
 // If accessed directly, bail
-if ( !defined('WP_UNINSTALL_PLUGIN') ) {
-	exit('Direct access is not allowed!');
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit( 'Direct access is not allowed!' );
 }
 
-// Plugin deleting functionality
-function kfn_delete_plugin()
-{
-	$defaults = kfn_get_option('defaults');
+/**
+ * Plugin deleting functionality
+ * -----------------------------------------------------------------------------------------------
+ * Deleting KFN plugin process contains two main parts:
+ *
+ * 1. Delete all plugin data from database ( "kfn" and "kfn_settings" rows in "wp_options" table )
+ * 2. Delete all posts with type "kfn"
+ */
+
+delete_option( 'kfn' );
+delete_option( 'kfn_settings' );
+
+$kfn_posts = get_posts( array(
+	'numberposts' => - 1,
+	'post_status' => 'any',
+	'post_type'   => 'kfn'
+) );
+
+foreach ( $kfn_posts as $post ) {
+	wp_delete_post( $post->ID, true );
 }
+
 
 
